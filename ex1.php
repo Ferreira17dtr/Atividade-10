0b9f7c7fa21c1ba41fe52de42a125b631b327aa1
Professor <Professor@WIN-MP3JADQD58A.local>
2020-10-23 11:28:50 +0100

    Update ex1.php, ex3.php, and 3 more files...

---
+++ ex1.php
@@ -0,0 +1,21 @@
+<?php
+
+$cores = array('vermelho ', 'azul ', 'verde ','amarelo ');
+echo $cores[0];
+echo $cores[1];
+echo $cores[2];
+echo $cores[3];
+
+$a = array('Tesla ', 2000, 3.0, 'BMW', 'Fiat');
+echo $a[1];
+
+$a[1]=2000;
+echo $a[1];
+
+$a[]="Vila das Aves";
+$a[]="AEDAH";
+foreach ($a as $item) {
+	echo $item. '<br>';
+}
+
+?>
